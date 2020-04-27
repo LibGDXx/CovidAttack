cb4b1795cee391a7c0b20e6845e2b895053fe4aa
Meghan Nahnybida <meghannahnybida@gmail.com>
2020-04-27 18:17:56 -0500

    Fixed sizes of every background

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,8 +7,8 @@ import com.mygdx.game.CovidAttack;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1500;
-		config.height = 750;
+		config.width = 500;
+		config.height = 500;
 		config.title = "CovidAttack";
 		new LwjglApplication(new CovidAttack(), config);
 	}
