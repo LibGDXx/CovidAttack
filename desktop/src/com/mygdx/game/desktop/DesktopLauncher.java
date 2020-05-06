dc29046016a3cb5e527f5b47bdc909906df14101
Meghan Nahnybida <meghannahnybida@gmail.com>
2020-05-06 09:49:43 -0500

    commented out start screen

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -3,7 +3,7 @@ package com.mygdx.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.CovidAttack;
-import com.mygdx.game.Start;
+//import com.mygdx.game.Start;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
@@ -11,6 +11,7 @@ public class DesktopLauncher {
 		config.width = 500;
 		config.height = 500;
 		config.title = "CovidAttack";
-		new LwjglApplication(new Start(), config);
+//		new LwjglApplication(new Start(), config);
+		new LwjglApplication(new CovidAttack(), config);
 	}
 }
