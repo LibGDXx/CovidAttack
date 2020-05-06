bd2b90c8dd1885d51ea006ab1634917e4fcbb345
Meghan Nahnybida <meghannahnybida@gmail.com>
2020-05-06 15:04:40 -0500

    fixing size of title screen

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -5,12 +5,10 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.CovidAttack;
 
 import javax.swing.*;
-//import com.mygdx.game.Start;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		//		new LwjglApplication(new Start(), config);
 		config.width = 500;
 		config.height = 500;
 		config.title = "CovidAttack";
