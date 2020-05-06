eac576359af88dafe49defb8368fe0b3b2b7de1f
Meghan Nahnybida <meghannahnybida@gmail.com>
2020-05-06 10:33:47 -0500

    adding temp menu box

--- core/src/com/mygdx/game/Die.java
+++ core/src/com/mygdx/game/Die.java
@@ -8,11 +8,12 @@ public class Die {
     public static void dieMessage(){
 
         int selection = JOptionPane.showConfirmDialog(null, "You died! Would you like to play again?",
-                "Game Over", JOptionPane.INFORMATION_MESSAGE);
+                "Game Over", JOptionPane.YES_NO_OPTION);
         boolean isYes = (selection == JOptionPane.YES_OPTION);
         boolean isNo = (selection == JOptionPane.NO_OPTION);
         if(isYes == true){
             //need to bring game back to title screen. not sure how yet.
+            //call DesktopLauncher somehow
 
         }
         else if(isNo){
