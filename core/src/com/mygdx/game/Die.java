7fa5a28a996c50bd728486052333ab0ad49c41e4
Meghan Nahnybida <meghannahnybida@gmail.com>
2020-05-08 10:27:03 -0500

    cleaning up code + adding more comments

--- core/src/com/mygdx/game/Die.java
+++ core/src/com/mygdx/game/Die.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 
 public class Die {
     public static void dieMessage(){
+        //if the user dies, they are prompted with a pop up window telling them they died, and then the game exits.
         int selection = JOptionPane.showConfirmDialog(null, "You died! Thanks for playing!",
                 "Game Over", JOptionPane.DEFAULT_OPTION);
         boolean isYes = (selection == JOptionPane.YES_OPTION);
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -9,7 +9,7 @@ import javax.swing.*;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		ImageIcon title = new ImageIcon("TitleScreen.png"); //change this path to whatever the absolute path of TitleScreen.png is on your computer
+		ImageIcon title = new ImageIcon("TitleScreen.png"); //starting screen background image
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 500;
 		config.height = 500;
@@ -19,15 +19,13 @@ public class DesktopLauncher {
 				"CovidAttack",   JOptionPane.YES_NO_OPTION);
 		boolean isYes = (selection == JOptionPane.YES_OPTION);
 		boolean isNo = (selection == JOptionPane.NO_OPTION);
-		boolean isPlay = (selection == JOptionPane.YES_OPTION);
+		//if the user selects the "yes" button, the CovidAttack() class is called
 		if(isYes == true){
 			new LwjglApplication(new CovidAttack(), config);
 		}
+		//if the user selects the "no" button, a second JOptionPane window will pop up, and exit the player out of the game.
 		else if(isNo == true){
 			JOptionPane.showMessageDialog(null, "Bye!", "CovidAttack", JOptionPane.PLAIN_MESSAGE);
-			if(isPlay ==  true){
-				new LwjglApplication(new CovidAttack(), config);
-			}
 		}
 	}
 }
