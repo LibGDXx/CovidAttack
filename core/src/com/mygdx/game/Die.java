5d7b3dca8d4525b6456b73e68e6233a3dd58873e
Alex Dawson <aad4wson@gmail.com>
2020-05-08 10:25:10 -0500

    fixed the question marks on the JOption pane boxes

--- core/src/com/mygdx/game/Die.java
+++ core/src/com/mygdx/game/Die.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class Die {
     public static void dieMessage(){
         int selection = JOptionPane.showConfirmDialog(null, "You died! Thanks for playing!",
-                "Game Over", JOptionPane.DEFAULT_OPTION);
+                "Game Over", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE);
         boolean isYes = (selection == JOptionPane.YES_OPTION);
         if(isYes){
             System.exit(0);
